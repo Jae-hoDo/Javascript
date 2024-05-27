e7822a605662d672384271c787fb5b97ff99b581
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-05-27 15:19:35 +0900

    update

--- Gameshop/js/index.js
+++ Gameshop/js/index.js
@@ -1,2 +1,9 @@
 const url = "http://localhost:8080/products";
 
+axios.get(url)
+.then((response)=>{
+    console.log("응답 Response: ", response);
+})
+.catch((error)=>{
+    console.log("에러 발생: ", error);
+});
